433e7c0e03da7961b5e41bf14e914217903e1729
Piyushh gargg <piyushgarg4445@gmail.com>
2026-03-09 23:30:18 +0530

    course

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://my-chat-app-production.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -81,6 +81,7 @@ export const useAuthStore = create((set, get) => ({
       set({ isUpdatingProfile: false });
     }
   },
+
 
   connectSocket: () => {
     const { authUser } = get();
