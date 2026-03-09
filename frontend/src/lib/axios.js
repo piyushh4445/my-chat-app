b8393a76886a202dc002374a2aabdfeaaf203776
Piyushh gargg <piyushgarg4445@gmail.com>
2026-03-09 22:39:56 +0530

    course

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://my-chat-app-eight-rho.vercel.app/",
   withCredentials: true,
 });
